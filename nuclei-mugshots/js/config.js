0aa88d6804f1427ef555a2bcc8d26a9c0614c0dd
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2016-05-03 10:57:36 -0400

    not quip_images

--- nuclei-mugshots/js/config.js
+++ nuclei-mugshots/js/config.js
@@ -9,7 +9,7 @@ var config = {
     quipUrl: 'http://quip.bmi.stonybrook.edu/camicroscope_latest/osdCamicroscope.php',
     reserve4Url: 'http://reserve4.informatics.stonybrook.edu/dev1/osdCamicroscope.php',
     analysis_execution_id: 'luad:20160215',
-    imgcoll: 'quip_images',
+    imgcoll: 'images',
     quot: "%22",
     //mongoUrl: 'mongodb://172.17.0.1:27015/u24_version3'
 };
