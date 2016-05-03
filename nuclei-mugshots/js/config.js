9a57b5e285d8103fd5941ea2fd2985387426d3de
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2016-05-03 10:44:14 -0400

    Update config.js

--- nuclei-mugshots/js/config.js
+++ nuclei-mugshots/js/config.js
@@ -3,12 +3,13 @@ console.log('config.js');
 var config = {
     domain: 'http://sbu-bmi.github.io/FeatureScapeApps',
     //domain: 'http://localhost:63342/FeatureScapeApps',
-    findAPI: 'https://falcon.bmi.stonybrook.edu',
-    port: 4500,
+    //findAPI: 'https://falcon.bmi.stonybrook.edu',
+    findAPI: 'http://quip1.uhmc.sunysb.edu',
+    port: 4000,
     quipUrl: 'http://quip.bmi.stonybrook.edu/camicroscope_latest/osdCamicroscope.php',
     reserve4Url: 'http://reserve4.informatics.stonybrook.edu/dev1/osdCamicroscope.php',
     analysis_execution_id: 'luad:20160215',
     imgcoll: 'quip_images',
     quot: "%22",
-    mongoUrl: 'mongodb://172.17.0.1:27015/u24_version3'
+    //mongoUrl: 'mongodb://172.17.0.1:27015/u24_version3'
 };
