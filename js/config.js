f2d1522095dbd61e1f8717623b0d616c8149e1d5
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2016-06-04 17:52:48 -0400

    update config.js

--- js/config.js
+++ js/config.js
@@ -3,19 +3,15 @@ console.log('config.js');
 var config = {
     domain: 'http://sbu-bmi.github.io/FeatureScapeApps',
     //domain: 'http://localhost:63342/FeatureScapeApps',
-    //findAPI: 'https://falcon.bmi.stonybrook.edu',
     findAPI: 'http://quip1.uhmc.sunysb.edu',
     port: 4000,
-    //quipUrl: 'http://quip.bmi.stonybrook.edu/camicroscope_latest/osdCamicroscope.php',
     quipUrl: 'http://quip1.uhmc.sunysb.edu/camicroscope/osdCamicroscope.php',
     reserve4Url: 'http://reserve4.informatics.stonybrook.edu/dev1/osdCamicroscope.php',
     default_execution_id: 'luad:bg:20160520',
-    default_db: 'u24_luad',    
+    default_db: 'u24_luad',
+    default_subject_id: 'TCGA-05-4244',    
     imgcoll: 'images',
     quot: "%22",
-    //iiifServer: 'quip.bmi.stonybrook.edu',
-    //iiifPrefix: 'fastcgi-bin/iipsrv.fcgi?iiif=',
     iiifServer: 'quip1.uhmc.sunysb.edu',
     iiifPrefix: 'fcgi-bin/iipsrv.fcgi?iiif='
-    //mongoUrl: 'mongodb://172.17.0.1:27015/u24_version3'
 };
