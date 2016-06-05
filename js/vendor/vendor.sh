21848169f55e9b9421f3edaf4bbc266d63af27ed
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2016-06-05 06:40:16 -0400

    css/js update

---
+++ js/vendor/vendor.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+wget http://www.w3schools.com/lib/w3data.js
+wget https://cdn.plot.ly/plotly-latest.min.js
+
+# https://code.jquery.com/
+wget https://code.jquery.com/jquery-2.2.4.min.js
+
+# https://www.bootstrapcdn.com/
+wget https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js
+
+# https://cdnjs.com/libraries/localforage
+wget https://cdnjs.cloudflare.com/ajax/libs/localforage/1.4.2/localforage.min.js
+#wget https://cdnjs.cloudflare.com/ajax/libs/localforage/1.3.1/localforage.min.js
+#wget https://cdnjs.cloudflare.com/ajax/libs/localforage/1.3.0/localforage.min.js
+
+# https://cdnjs.com/libraries/d3
+wget https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.min.js
+
+# https://cdnjs.com/libraries/crossfilter
+wget https://cdnjs.cloudflare.com/ajax/libs/crossfilter/1.3.12/crossfilter.min.js
+
+# https://cdnjs.com/libraries/dc
+wget https://cdnjs.cloudflare.com/ajax/libs/dc/1.7.5/dc.min.js
+#wget https://dc-js.github.io/dc.js/js/dc.js
