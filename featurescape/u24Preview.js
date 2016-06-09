a334ccbe5c1b0a22acb18e67ca2f4d978def032e
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2016-06-09 14:31:18 -0400

    table

--- featurescape/u24Preview.js
+++ featurescape/u24Preview.js
@@ -11,28 +11,50 @@ u24p.buildUI = function (dataOriginDivId, dataDivId, data) { // build User Inter
     var dataDiv = document.getElementById(dataDivId);
     dataDiv.innerHTML = '<h4>' + data.length + ' ' + (selected.cancer_type).toUpperCase() + ' Diagnostic Images:</h4>';
 
-    var ol = document.createElement('ol');
-    dataDiv.appendChild(ol);
+    var tbl = document.createElement('table');
+    tbl.cellPadding = "10";
+    dataDiv.appendChild(tbl);
 
     data.forEach(function (c) {
 
         tissueId = c.image.case_id;
 
-        var li = document.createElement('li');
-        ol.appendChild(li);
+        var row = document.createElement('tr');
+        tbl.appendChild(row);
 
-        li.innerHTML = '<a href="' + abcUtil.caMicroLink(tissueId, selected.cancer_type) + '" target="_blank">' + tissueId + '</a>, '
-            + '(<a href="http://www.cbioportal.org/case.do?cancer_study_id=' + c.provenance.study_id
-            + '_tcga&case_id=' + c.image.subject_id + '" target="_blank" style="color:red">cbio</a>), ';
+        // caMicro
+        var link = document.createElement('a');
+        link.setAttribute("href",
+            abcUtil.caMicroLink(tissueId, selected.cancer_type));
+        link.setAttribute("target", "_blank");
+        link.innerHTML = tissueId + "&nbsp;&nbsp;";
 
+        var col = document.createElement('td');
+        col.appendChild(link);
+        row.appendChild(col);
+
+        // cBio
         // Note: TCGA's case_id parm actually refers to the patient ("subject"); not the case_id.
         // eg. http://www.cbioportal.org/case.do?cancer_study_id=luad_tcga&case_id=TCGA-05-4395
+        link = document.createElement('a');
+        link.setAttribute("href",
+            'http://www.cbioportal.org/case.do?cancer_study_id='
+            + c.provenance.study_id
+            + '_tcga&case_id=' + c.image.subject_id);
+        link.setAttribute("target", "_blank");
+        link.style.color = 'red';
+        link.innerHTML = 'cbio&nbsp;&nbsp;';
 
+        col = document.createElement('td');
+        col.appendChild(link);
+        row.appendChild(col);
+
+        // FeatureScape
         var btFeature = document.createElement('button');
-        li.appendChild(btFeature);
+        col = document.createElement('td');
+        col.appendChild(btFeature);
         btFeature.textContent = "FeatureScape of sampled features";
         btFeature.style.color = "blue";
-        // TODO: class="btn btn-secondary"
 
         btFeature.onclick = function () {
             var sz = 1000;
@@ -41,7 +63,8 @@ u24p.buildUI = function (dataOriginDivId, dataDivId, data) { // build User Inter
             var url = config.domain + '/featurescape/?' + config.findAPI + ':' + config.port + '?limit=' + sz + '&find={"randval":{"$gte":' + v + '},"provenance.analysis.execution_id":"' + selected.execution_id + '","provenance.image.case_id":"' + tissueId + '"}&db=' + selected.db;
             window.open(url);
 
-        }
+        };
+        row.appendChild(col);
     })
 
 };
