be72f074272de874c5be7d11fecf41ab31b2d505
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2016-11-15 10:09:10 -0500

    quip1-new

--- js/config-old.js
+++ js/config-old.js
@@ -3,13 +3,14 @@
  */
 var config = {
     domain: 'http://sbu-bmi.github.io/FeatureScapeApps',
-    findAPI: 'http://quip1-new.uhmc.sunysb.edu',
-    port: 3000,
-    quipUrl: 'http://quip1-new.uhmc.sunysb.edu/camicroscope/osdCamicroscope.php',
+    //domain: 'http://localhost:63342/FeatureScapeApps',
+    findAPI: 'http://quip1.bmi.stonybrook.edu',
+    port: 4000,
+    quipUrl: 'http://quip1.bmi.stonybrook.edu/camicroscope/osdCamicroscope.php',
     reserve4Url: 'http://reserve4.informatics.stonybrook.edu/dev1/osdCamicroscope.php',
     imgcoll: 'images',
     quot: "%22",
-    iiifServer: 'quip1-new.uhmc.sunysb.edu',
+    iiifServer: 'quip1.bmi.stonybrook.edu',
     iiifPrefix: 'fcgi-bin/iipsrv.fcgi?iiif=',
     default_execution_id: 'luad:bg:20160520',
     default_db: 'u24_luad',
